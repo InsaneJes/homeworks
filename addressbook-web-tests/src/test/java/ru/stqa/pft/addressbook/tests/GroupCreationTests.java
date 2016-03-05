253875bafb8072032a52212d7047dc18565a9ac0
InsaneJes <insane.jes@gmail.com>
2016-03-05 04:11:53 +0300

    Реализована проверка и выполнение предусловий для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,11 +9,7 @@ public class GroupCreationTests extends TestBase{
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
-        app.getGroupHelper().initGroupCreation();
-        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
-        app.getGroupHelper().submitGroupCreation();
-        app.getGroupHelper().returnToGroupPage();    /* Т.к. метод возвращения через эту ссылку, судя по всему,
-                                     всё-таки понадобится, то вернул его обратно =) */
+        app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
     }
 
 }
