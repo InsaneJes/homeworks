cffccfcb8261bcb378e8c97e232fd105d76485e3
InsaneJes <insane.jes@gmail.com>
2016-03-31 18:52:35 +0300

    Добавлена загркзка тестовых данных из файла
    Для групп и контактов.
    + добавлены два файла с данными

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/UserCreationTests.java
@@ -7,7 +7,7 @@ import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.UserData;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -18,17 +18,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class UserCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validUsers() {
+    public Iterator<Object[]> validUsers() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[] {new UserData().withFirstname("Ivan 1").withLastname("Ivanov 1").withAddress1("pr-t Mira, 1").withAddress2("pr-t Pobedi 1")
-                .withUsermail("Test@mail.org1").withEmail2("Test2@mail.org1").withEmail3("Test3@mail.org1").withHomePhone("01").withMobilePhone("001").withWorkPhone("0001")
-                .withPhoto(new File("src/test/resources/ph.png"))});
-        list.add(new Object[] {new UserData().withFirstname("Ivan 2").withLastname("Ivanov 2").withAddress1("pr-t Mira, 2").withAddress2("pr-t Pobedi 2")
-                .withUsermail("Test@mail.org2").withEmail2("Test2@mail.org2").withEmail3("Test3@mail.org2").withHomePhone("02").withMobilePhone("002").withWorkPhone("0002")
-                .withPhoto(new File("src/test/resources/ph.png"))});
-        list.add(new Object[] {new UserData().withFirstname("Ivan 3").withLastname("Ivanov 3").withAddress1("pr-t Mira, 3").withAddress2("pr-t Pobedi 3")
-                .withUsermail("Test@mail.org3").withEmail2("Test2@mail.org3").withEmail3("Test3@mail.org3").withHomePhone("03").withMobilePhone("003").withWorkPhone("0003")
-                .withPhoto(new File("src/test/resources/ph.png"))});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/users.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new UserData().withFirstname(split[0]).withLastname(split[1]).withAddress1(split[2]).withAddress2(split[3])
+                    .withUsermail(split[4]).withEmail2(split[5]).withEmail3(split[6]).withHomePhone(split[7]).withMobilePhone(split[8]).withWorkPhone(split[9])});
+            line = reader.readLine();
+        }
         return list.iterator();
     }
 
